4e149374de1ec0b7a508931ae714679ed369e3ef
tinf1213 <ericding1213@gmail.com>
2022-07-27 17:14:34 -0700

    start e

--- atcoder/abc261/e.cpp
+++ atcoder/abc261/e.cpp
@@ -3,9 +3,18 @@ using namespace std;
 #define iou ios_base::sync_with_stdio(0), cin.tie(0);
 #define int long long
 
+int data1[200000]; 
+int data2[200000]; 
+
 signed main(){
     int n,c; cin >> n >> c;
-    cout << n and c;
-
+    for(int i=0;i,n;i++) cin >> data1[i] >> data2[i];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(data1[i] == 1) cout << c & data2[i] << endl;
+            else if(data1[i] == 2) cout << c | data2[i] << endl;
+            else cout << c ^ data2[i] << endl;
+        }
+    }
     return 0;
 }
