6f47870de8c29c9fc50483a913287d9cf11708df
丁時揚 <88125346+tinf1213@users.noreply.github.com>
2022-08-17 16:23:06 +0800

    abc264

---
+++ atcoder/abc264/a.py
@@ -0,0 +1,6 @@
+string = "atcoder"
+a = input().split()
+b = int(a[1])
+a = int(a[0])
+for i in range(a-1,b):
+    print(string[i], end = '')
---
+++ atcoder/abc264/b.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define iou ios_base::sync_with_stdio(0), cin.tie(0);
+#define int long long
+
+signed main(){
+    iou;
+    int a,b; cin >> a >> b;
+    int temp, add, color;
+    bool status1, status2;
+    if(a > 7 or b > 7) status1 = false;
+    else status1 = true;
+    if(a > b){
+        status2 = true;
+        temp = b;
+        add = a - b;
+    } 
+    else{
+        status2 = false;   
+        temp = a;
+        add = b - a;
+    } 
+    if(temp % 2 == 0) color = 1;
+    else color = 0;
+    int reserve = 15 - temp;
+    if(status2){
+        if(temp > add){
+            if ((add - temp) % 2 == 1) color *= -1;
+        }
+    }
+    else{
+        if(add % 2 == 1) color *= -1;
+    }
+    if(color == 1){
+        cout << "black" << endl;
+    }
+    else{
+        cout << "white" << endl;
+    }
+    return 0;
+}
