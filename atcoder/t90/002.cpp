6b02eb837fe0987c59a4462b71fd36abc78d2248
tinf1213 <ericding1213@gmail.com>
2022-08-19 22:22:01 +0800

    t90 002

--- atcoder/t90/002.cpp
+++ atcoder/t90/002.cpp
@@ -4,13 +4,36 @@ using namespace std;
 #define int long long
 int n;
 
+bool verify(string temp){
+    int counter = 0;
+    for(int i=0;i<temp.size();i++){
+        if(temp[i] == '(') counter++;
+        else counter--;
+        if(counter < 0) return false;
+        //cout << counter << endl;
+    }
+    if(counter == 0) return true;
+    return false;
+}
+
+
 signed main(){
     cin >> n;
     if (n%2 != 0) return 0;
-    int bin = 0x00;
+    int bin = 0b00;
     for(int i=n;i>n/2;i--) bin += 1 << i-1;
     bin << n/2;
-    int *ptr = &bin;
-    cout <<  << endl;
+    for(int i=bin;i>=0;i--){
+        string temp = "";
+        for(int j=n-1;j>=0;j--){
+            if(i & (1 << j)) temp += '(';
+            else temp += ')';
+        }
+        //cout << i << endl;
+        //cout << temp << endl;
+        bool flag = verify(temp);
+        if(flag) cout << temp << endl;
+        //cout << flag << endl;
+    }
     return 0;
 }
