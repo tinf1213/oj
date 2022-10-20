05f1f296b423ae5fdf5fadb290a293af66107a7c
tinf1213 <ericding1213@gmail.com>
2022-10-20 17:08:08 +0800

    ubuntu

--- atcoder/abc241/c.cpp
+++ atcoder/abc241/c.cpp
@@ -2,62 +2,63 @@
 using namespace std;
 #define io ios_base::sync_with_stdio(0), cin.tie(0);
 #define int long long
+#define yes cout << "Yes" << endl; return 0;
+#define no cout << "No" << endl;
+int n;
 
 signed main(){
     io;
-    int n; cin >> n;
-    vector<vector<bool>> mp;
+    cin >> n;
+    vector<string> mp;
+    string temp;
     for(int i=0;i<n;i++){
-        vector<bool> temp;
-        string input; cin >> input;
-        for(int j=0;j<n;j++){
-            if(input[j] == '.') temp.push_back(0);
-            else temp.push_back(1);
-        }
+        cin >> temp;
         mp.push_back(temp);
     }
-
-    bool flag = 0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            int cnt = 0;
-            if(j + 6 <= n and i + 6 <= n){
-                for(int k=0;k<6;k++){
-                    cnt += mp[i+k][j+k];
+            if(i+5 < n){
+                int cnt = 0;
+                for(int k=1;k<=5;k++) if(mp[i+k][j] == '#') cnt++;
+                if(cnt>=4) yes;
+                if(j+5 < n){
+                    cnt = 0;
+                    for(int k=0;k<=5;k++) if(mp[i+k][j+k] == '#') cnt++;
+                    if(cnt>=4) yes;
+                } 
+                if(j-5 >= 0){
+                    cnt = 0;
+                    for(int k=0;k<=5;k++) if(mp[i+k][j-k] == '#') cnt++;
+                    if(cnt>=4) yes;
                 }
-                if(cnt >= 4) flag = 1;
-                //cout << "a " << cnt << endl;
             }
-            cnt = 0;
-            if(i + 6 <= n and !flag){
-                for(int k=0;k<6;k++){
-                    cnt += mp[i+k][j];
+            if(i-5 >= 0){
+                int cnt = 0;
+                for(int k=0;k<=5;k++) if(mp[i-k][j] == '#') cnt++;
+                if(cnt>=4) yes;
+                if(j + 5 < n){
+                    cnt = 0;
+                    for(int k=0;k<=5;k++) if(mp[i-k][j+k] == '#') cnt++;
+                    if(cnt>=4) yes;
+                } 
+                if(j-5 >= 0){
+                    cnt = 0;
+                    for(int k=0;k<=5;k++) if(mp[i-k][j-k] == '#') cnt++;
+                    if(cnt>=4) yes;
                 }
-                if(cnt >= 4) flag = 1;
-                //cout << "b " << cnt << endl;
             }
-            cnt = 0;
-            if(j + 6 <= n and !flag){
-                for(int k=0;k<6;k++){
-                    cnt += mp[i][j+k];
-                }
-                if(cnt >= 4) flag = 1;
-                //cout << "c " << cnt << endl;
-            }       
-            if(j + 6 <= n and !flag and i - 6 >= 0){
-                for(int k=0;k<6;k++){
-                    cnt += mp[i-k][j+k];
-                }
-                if(cnt >= 4) flag = 1;
-                //cout << "c " << cnt << endl;
-            }    
-            if(flag) break;
+            if(j+5 < n){
+                int cnt = 0;
+                for(int k=0;k<=5;k++) if(mp[i][j+k] == '#') cnt++;
+                if(cnt>=4) yes;
+            }
+            if(j-5 >= 0){
+                int cnt = 0;
+                for(int k=0;k<=5;k++) if(mp[i][j-k] == '#') cnt++;
+                if(cnt>=4) yes;
+            }
         }
-        if(flag) break;
     }
-    if(flag) cout << "Yes" << endl;
-    else cout << "No" << endl;
-    
+    cout << "No" << endl;
     return 0;
-    
 }
