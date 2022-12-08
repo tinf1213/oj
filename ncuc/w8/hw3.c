542da66bf3cf1316ada64d0a4bc82e0c84a4638a
tinf1213 <ericding1213@gmail.com>
2022-12-08 17:58:04 +0800

    hw3.c

--- ncuc/w8/hw3.c
+++ ncuc/w8/hw3.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
+int n;
+char input[1<<18];
+
+void cin(){
+    scanf("%d", &n);
+    fgets(input, 1<<18, stdin);
+}
+
 int main(){
-    
+    while(1){
+        cin();
+        
+    }
 }
