efc62eb3da18de3903be07005323a9b29787da46
Tinf1213 <88125346+tinf1213@users.noreply.github.com>
2022-12-28 21:35:05 +0800

    fix w9 hw3.c

--- ncuc/w9/hw3.c
+++ ncuc/w9/hw3.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 int main(){
 	int n; scanf("%d", &n);
-	int row = n / 702;
+	int row = n / 703;
 	printf("%d-", row+1);
 	int col = n % 702;
-	if(col <= 26) printf("%c\n", (char)(col+64));
-	else {
-		printf("%c", (char)(col/26+64));
-		col %= 26;
-		printf("%c\n", (char)(col+64));
+	if(col == 0){
+		printf("%s", "ZZ")	;
+	}
+	else{
+		if(col <= 26) printf("%c\n", (char)(col+64));
+		else {
+			printf("%c", (char)(col/26+64));
+			col %= 26;
+			printf("%c\n", (char)(col+64));
+		}
 	}
 	return 0;
 }
